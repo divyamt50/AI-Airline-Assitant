504b3e8162d375966bb5699ca44f00458600d839
divyamt50 <divyamt50@gmail.com>
2026-07-02 14:42:45 +0530

    Add database module

---
+++ database.py
@@ -0,0 +1,26 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from collections.abc import AsyncGenerator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_URL_CONNECT=f"postgresql+psycopg2://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+
+engine = create_async_engine(
+    pool_size = 10,
+    max_overflow = 20,
+    pool_recycle = 1800,
+    pool_pre_ping = True,
+    echo = False
+)
+
+SessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_session()->AsyncGenerator[AsyncSession, None]:
+    async with SessionLocal() as session:
+        yield session
